c8768694bfda44e22171051b72d2c1b1ed8ca8a7
Emil Lundberg <lundberg.emil@gmail.com>
2014-10-18 17:26:09 +0200

    Add jquery to test environment

--- validator/karma.conf.js
+++ validator/karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jquery-2.1.0', 'jasmine'],
 
 
     // list of files / patterns to load in the browser
