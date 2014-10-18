747923a5aae4e7776693a109ae27261a2932f6d0
Emil Lundberg <lundberg.emil@gmail.com>
2014-10-18 17:29:01 +0200

    Add test for lastupdated at /status

--- validator/spec/test.js
+++ validator/spec/test.js
@@ -1,4 +1,14 @@
-describe('A test', function() {
-	it('should succeed', function() {
-	});
+describe('The datasucker at ' + targetBaseUrl, function() {
+    describe('has a /status endpoint', function() {
+        var data = $.ajax(targetBaseUrl + '/status', {
+            async: false,
+            dataType: 'json',
+        }).responseJSON;
+
+        it('which contains a "lastupdated" full ISO8601 timestamp', function() {
+            var lastupdatedRestringified = new Date(data.lastupdated).toISOString();
+            expect(lastupdatedRestringified).not.toEqual('Invalid Date');
+            expect(lastupdatedRestringified).toEqual(data.lastupdated);
+        });
+    });
 });
