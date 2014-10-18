16c72ab1c79981730be49bf6d43caeb3578087a9
Emil Lundberg <lundberg.emil@gmail.com>
2014-10-18 17:36:03 +0200

    Cache the data for each API path and reset it before each spec

--- validator/spec/test.js
+++ validator/spec/test.js
@@ -1,9 +1,19 @@
 describe('The datasucker at ' + targetBaseUrl, function() {
-    describe('has a /status endpoint', function() {
-        var data = $.ajax(targetBaseUrl + '/status', {
+
+    var data;
+    function getData(apiPath) {
+        var cachedData = $.ajax(targetBaseUrl + apiPath, {
             async: false,
             dataType: 'json',
         }).responseJSON;
+
+        return function() {
+            data = cachedData;
+        };
+    }
+
+    describe('has a /status endpoint', function() {
+        beforeEach(getData('/status'));
 
         it('which contains a "lastupdated" full ISO8601 timestamp', function() {
             var lastupdatedRestringified = new Date(data.lastupdated).toISOString();
