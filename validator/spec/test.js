70c8153dcf2b13ab8854d46b3d65fc0635935a53
Emil Lundberg <lundberg.emil@gmail.com>
2014-10-20 13:20:31 +0200

    Test that the datasucker has Datasucker with the mandatory keys

--- validator/spec/test.js
+++ validator/spec/test.js
@@ -78,4 +78,48 @@ describe('The datasucker at ' + testParams.targetBaseUrl, function() {
             data.length.should.be.greaterThan(0);
         });
     });
+
+    describe('has the Datasucker card (/card/01008 endpoint) which', function() {
+        beforeEach(getData('/card/01008'));
+
+        var mandatoryKeys = [
+            'code',           // (/string => last 5 digits of the GUIDs, ex: "01023" => set 01, card 023)
+            'faction',        // (/string)
+            'factioncost',    // (/integer)
+            'images',         // (/array => may be more than one if the card has alternate art, primary art is index 0)
+               //illustrator, // (/string)
+               //src,         // (/string)
+            'number',         // (/integer => number within the set)
+            'maxperdeck',     // (/integer => how many of this card are allowed in a deck)
+            'quantity',       // (/integer => how many of this card are in the set/pack)
+            'regex',          // (/string => regular expression pattern for matching cards with varying text)
+            'set',            // (/string => name of the set/datapack)
+            'setcode',        // (/string => internal code for set, not suitable for sorting purposes)
+            'side',           // (/string => "Runner" or "Corp")
+            'subtype',        // (/string => if set, list of card subtypes separated by ' - ')
+            'text',           // (/string)
+            'title',          // (/string)
+            'type',           // (/string)
+            'url',            // (/string => card game DB spoiler URL)
+        ];
+
+        var otherKeys = [
+            'flavor',         // (/string)
+            'cost',           // (/integer)
+            'agendapoints',   // (/integer => only used if card is an agenda)
+            'baselink',       // (/integer => only used if card is a runner identity)
+            'influencelimit', // (/integer => only used if card is an identity)
+            'memoryunits',    // (/integer => only used if card is a runner program)
+            'mindecksize',    // (/integer => only used if card is an identity)
+            'strength',       // (/integer)
+            'trash',          // (/integer)
+            'uniqueness',     // (/boolean)
+        ];
+
+        _(mandatoryKeys).each(function(key) {
+            it('has the ' + key + ' property', function() {
+                data.should.have.property(key);
+            });
+        });
+    });
 });
