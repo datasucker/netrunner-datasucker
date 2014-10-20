b61ac00a2cff1efe986c12c6956daad405f79016
Emil Lundberg <lundberg.emil@gmail.com>
2014-10-20 11:42:02 +0200

    Add description to targetBaseUrl test parameter

--- validator/test-params.js
+++ validator/test-params.js
@@ -1,3 +1,6 @@
 module.exports = {
+    // The base URL of the target datasucker, without trailing slash. If this
+    // is 'http://foo.org', then the tests will check for example that the
+    // target serves card data at 'http://foo.org/cards'.
     targetBaseUrl: 'http://localhost:8080',
 };
