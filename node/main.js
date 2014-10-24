a0fc0c5d164ae4aa6f53897a97ac1645516dfab8
Emil Lundberg <lundberg.emil@gmail.com>
2014-10-24 15:26:30 +0200

    Keep cards in a Backbone.Collection

--- node/main.js
+++ node/main.js
@@ -1,3 +1,4 @@
+var Backbone = require('backbone');
 var express = require('express');
 var _ = require('underscore');
 var mapCard = require('./cgdb-utils').mapCard;
@@ -10,6 +11,11 @@ var sideLoadImages = true;
 
 var app = express();
 app.set('jsonp callback', true);
+
+var Card = Backbone.Model.extend({ idAttribute: 'code' });
+var CardList = Backbone.Collection.extend({ model: Card });
+
+var cards = new CardList();
 
 function addRoute(path, jsonBuilder) {
 	return app.get(path, function(req, res) {
@@ -33,7 +39,7 @@ addRoute('/cards', function(req) {
 // Get a card's JSON data by its code (ex: "01024")
 addRoute('/card/:code', function(req) {
 	var code = req.params.code;
-	return _.findWhere(cards, { code: code });
+	return cards.get(code);
 });
 
 var server = app.listen(8080, function() {
@@ -42,36 +48,3 @@ var server = app.listen(8080, function() {
 
 	console.log('Datasucker listening at ' + host + ' port ' + port);
 });
-
-var cards = [
-    {
-      "code": "01008",
-      "cost": 1,
-      "faction": "Anarch",
-      "factioncost": 1,
-      "maxperdeck": 3,
-      "memoryunits": 1,
-      "number": 8,
-      "quantity": 2,
-      "regex": "(Datasuckers?|Data Suckers?|01008)",
-      "set": "Core",
-      "setcode": "223",
-      "side": "Runner",
-      "subtype": "Virus",
-      "text": "<redacted>",
-      "title": "Datasucker",
-      "type": "Program",
-      "uniqueness": false,
-      "url": "http:\/\/www.cardgamedb.com\/index.php\/netrunner\/android-netrunner-card-spoilers\/_\/datasucker-core",
-      "images": [
-	{
-	  "src": "<redacted>",
-	  "illustrator": "Chelsea Conlin"
-	},
-	{
-	  "src": "<redacted>",
-	  "illustrator": "Ed Mattinian"
-	}
-      ]
-    }
-];
